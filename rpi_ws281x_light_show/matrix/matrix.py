7befb07f5094805db06b107c9328d22cf818de4a
Riccardo <riccardo.cereghino@gmail.com>
2019-12-22 22:58:48 +0100

    Update

--- rpi_ws281x_light_show/matrix/matrix.py
+++ rpi_ws281x_light_show/matrix/matrix.py
@@ -11,7 +11,7 @@ class Matrix:
         self.led_pin = 18  # GPIO pin connected to the pixels (18 uses PWM!).
         self.led_freq_hz = 800000  # LED signal frequency in hertz (usually 800khz)
         self.led_dma = 10  # DMA channel to use for generating signal (try 10)
-        self.led_brightness = 127  # Set to 0 for darkest and 255 for brightest
+        self.led_brightness = 255  # Set to 0 for darkest and 255 for brightest
         self.led_invert = False  # True to invert the signal (when using NPN transistor level shift)
         self.led_channel = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rpi_ws281x_light_show',
-      version='0.1',
+      version='0.1.5',
       description='Cool animations for led matrixes of the family rpi_ws281x on Raspbian',
       long_description="Cool animations for led matrixes of the family rpi_ws281x on Raspbian",
       keywords='raspberry raspbian matrix rpi_ws281x animations',
