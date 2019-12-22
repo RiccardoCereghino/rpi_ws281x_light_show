9c0b0bdb63e52c75b81dfbee33bc3e6faf75ab2a
Riccardo <riccardo.cereghino@gmail.com>
2019-12-23 00:05:18 +0100

    :D

--- rpi_ws281x_light_show/matrix.py
+++ rpi_ws281x_light_show/matrix.py
@@ -10,7 +10,7 @@ class Matrix:
         self.led_pin = 18  # GPIO pin connected to the pixels (18 uses PWM!).
         self.led_freq_hz = 800000  # LED signal frequency in hertz (usually 800khz)
         self.led_dma = 10  # DMA channel to use for generating signal (try 10)
-        self.led_brightness = 63  # Set to 0 for darkest and 255 for brightest
+        self.led_brightness = 15  # Set to 0 for darkest and 255 for brightest
         self.led_invert = False  # True to invert the signal (when using NPN transistor level shift)
         self.led_channel = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
