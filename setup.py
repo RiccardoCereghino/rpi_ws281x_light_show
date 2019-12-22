364a92eb1b7267eb24e9b908e0ef30f855597887
Riccardo <riccardo.cereghino@gmail.com>
2019-12-22 15:56:04 +0100

    pip barebone project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='rpi_ws281x_light_show',
+      version='0.0',
+      description='Cool animations for led matrixes of the family rpi_ws281x on Raspbian',
+      long_description="Cool animations for led matrixes of the family rpi_ws281x on Raspbian",
+      keywords='raspberry raspbian matrix rpi_ws281x animations',
+      url='http://github.com/RiccardoCereghino/rpi_ws281x_light_show',
+      author='Riccardo Cereghino',
+      author_email='riccardo.cereghino@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False
+      )
